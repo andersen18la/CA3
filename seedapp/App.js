1310f44de56ed3ee61913efe21da71267bbcccc3
Hallur20 <hvn15@hotmail.com>
2017-11-03 15:07:29 +0100

    get info on app

--- seedapp/App.js
+++ seedapp/App.js
@@ -1,27 +1,56 @@
 import React from 'react';
-import all from "./facades/fetchfacade";
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+const URL = 'https://jdbh.dk/backend/CA3Group4Server/';
 export default class App extends React.Component {
 
-  getAll() {
-    Alert.alert(all.getAll);
+  constructor(){
+    super();
+    this.state = {data : []};
+    this.getAll = this.getAll.bind(this);
   }
+
+  getAll() {
+    fetch(URL + "api/places/all")
+      .then((response) => {
+        return response.json()})
+      .then((responseJson) => {
+        var arr = responseJson;
+          this.setState({data : responseJson});
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  
   render() {
+    const tableHead = ['id', 'city', 'description' , 'geo', 'imageuri', 'rating', 'street', 'zip'];
+
+    const mapped = this.state.data.map(function(element)
+    { return [element.id, element.city, element.description, element.geo, element.imageuri, element.rating, element.street, element.zip] })
+
     return (
-      <View style={styles.container}>
-        <Button onPress={this.getAll} title="getinfo" />
+
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Button  onPress={this.getAll} title="getinfo" />
         <Text>Open up App.js to start working on your app!</Text>
+       
+ 
+        <Table>
+          <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
+          <Rows data={mapped} style={styles.row} textStyle={styles.text}/>
+          
+        </Table>
+ 
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { marginLeft: 1 },
+  row: { height: 30 }
 });
