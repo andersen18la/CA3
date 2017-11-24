6424dd141d4dc9aaadb93cc055407b0c2e1aa068
Hallur20 <hvn15@hotmail.com>
2017-11-24 11:11:59 +0100

    map pinpoints works

--- seedClient/src/pages/Rating.js
+++ seedClient/src/pages/Rating.js
@@ -6,6 +6,10 @@ import placeData from '../facades/placeFacade';
  * userId
  * placeId
  * updateTable
+ * Stjerner... eller runde tingester som man klikke på.
+ * <form bla>
+ * <input name="radio1" type="radio" value="1" onClick={this.onClickHandler}
+ * </form>
  */
 
 
@@ -19,13 +23,22 @@ export default class Rating extends Component {
     }
     submitHandler = e => {
         e.preventDefault();
-        let rating = this.state.rating;        
+        let rating = this.state.rating;
         placeData.createRating(rating, this.props.updateTable);
         this.setState({
             rating: { userId: this.props.userId, ratingValue: "0", placeId: this.props.placeId }
         });
     }
 
+    onClickHandler = e => {        
+        let value = e.target.value;
+        console.log("value " + value);
+        this.setState(prevState => (
+            {
+                rating: { ...prevState.rating, ratingValue: value },
+            }
+        ));
+    }
     onChangeHandler = e => {
         e.preventDefault();
         let target = e.target;
@@ -39,13 +52,23 @@ export default class Rating extends Component {
         ));
     }
 
+    //<input name={this.props.placeId} className="form-control" type="number" onChange={this.onChangeHandler} value={this.state.rating.ratingValue} min="1" max="5" required />
     render() {
 
         return (
+            <div>
             <form key={this.props.placeId} className="form-inline" onSubmit={this.submitHandler}>
-                <input name={this.props.placeId} className="form-control" type="number" onChange={this.onChangeHandler} value={this.state.rating.ratingValue} min="1" max="5" required />
+                
+                <label><input className="form-inline" name="radio" type="radio" value="1" onClick={this.onClickHandler}/>1</label>           
+                <label><input className="form-inline" name="radio" type="radio" value="2" onClick={this.onClickHandler}/>2</label>           
+                <label><input className="form-inline" name="radio" type="radio" value="3" onClick={this.onClickHandler}/>3</label>           
+                <label><input className="form-inline" name="radio" type="radio" value="4" onClick={this.onClickHandler}/>4</label>           
+                <label><input className="form-inline" name="radio" type="radio" value="5" onClick={this.onClickHandler}/>5</label>           
+                
+
                 <input type="submit" className="btn btn-default" id="btn" value="Save the rating" />
             </form>
+            </div>
         )
 
     }
