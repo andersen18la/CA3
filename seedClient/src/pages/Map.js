3ea27156fbcac7b070526a73799f6be437c657d2
MO Kayed <marik.3000@hotmail.com>
2017-11-28 10:23:05 +0100

    geo loc + modal

--- seedClient/src/pages/Map.js
+++ seedClient/src/pages/Map.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import geocoder from 'geocoder'
 import Places from './Places'
 import placeData from '../facades/placeFacade'
+import Modal from 'react-modal';
+import PlaceForm from "./PlaceForm";
 const URL = require("../../package.json").serverURL;
 
 
@@ -20,12 +22,21 @@ const AnyReactComponent = ({ text, alerthis }) => (
     </div>
 
 );
-
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  };
 
 export default class Map extends Component {
     constructor(props) {
         super(props);
-        this.state = { city: "null", lat: 0.0, lng: 0.0, cities: [], dependencies: [] }
+        this.state = { city: "null", lat: 0.0, lng: 0.0, cities: [], dependencies: [],data: [], err: "", isOpen: false, modalIsOpen: false }
         this.getCoords = this.getCoords.bind(this);
         this.goToPlace = this.goToPlace.bind(this);
         this.alerthis = this.alerthis.bind(this);
@@ -33,6 +44,9 @@ export default class Map extends Component {
         this.dataStuff = this.dataStuff.bind(this);
         this.splitTest = this.splitTest.bind(this);
         this.showLocations = this.showLocations.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     static defaultProps = {
@@ -122,6 +136,40 @@ showLocations(){
             }, 700);
         }
     }
+    
+  componentWillMount() {
+    /*
+    This will fetch data each time you navigate to this route
+    If only required once, add "logic" to determine when data should be "refetched"
+    */
+    placeData.getData((e, data) => {
+      if (e) {
+        return this.setState({ err: e.err })
+      }
+      this.setState({ err: "", data, modalIsOpen: false });
+    });
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  updateTable = () => {
+    placeData.getData((e, data) => {
+      if (e) {
+        return this.setState({ err: e.err })
+      }
+      this.setState({ err: "", data, modalIsOpen: false });
+    });
+  }
     render() {
         const mapped = this.state.dependencies.map(function (element) { return <AnyReactComponent lat={element.lat} lng={element.lng} text={element.city} /> })
         return (
@@ -154,8 +202,30 @@ showLocations(){
                 <button onClick={this.showLocations}>make locations appear</button>
                 <p id="cities"></p>
 
-
+                <div id="modal" className="container">
+          <button className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" onClick={this.openModal}>Add Location</button>
+          <div className="modal fade" id="myModal" role="dialog">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <Modal
+                  isOpen={this.state.modalIsOpen}
+                  onAfterOpen={this.afterOpenModal}
+                  onRequestClose={this.closeModal}
+                  style={customStyles}
+                  contentLabel="Example Modal"
+                >
+                  <div>
+                    <div className="modal-header"><h2 ref={subtitle => this.subtitle = subtitle}>Example Text For Add Location</h2></div>
+                    <div className="modal-body">
+                      <PlaceForm onAddPlace={this.onAddPlace} onCloseModal={this.closeModal} updateTable={this.updateTable} /></div>
+                    <div className="modal-footer"><button className="btn btn-danger" onClick={this.closeModal}>close</button></div></div>
+                </Modal>
+              </div>
             </div>
+          </div>
+        </div>
+            </div>
+            
         );
     }
 }
